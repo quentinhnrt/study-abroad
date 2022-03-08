61fd73f96970ecbdd8bedef76832602d4bb67bbb
Luana Kimley <luanakimley@gmail.com>
2022-03-08 10:39:50 +0100

    delete & new article

--- client/src/articles/NewArticle.js
+++ client/src/articles/NewArticle.js
@@ -3,36 +3,42 @@ import axios from "axios";
 
 export default function NewArticle() {
   const [thumbnail, setThumbnail] = useState([]);
+  const [media, setMedia] = useState([]);
 
   let postArticle = (e) => {
     e.preventDefault();
     let title = document.getElementById("title").value;
     let content = document.getElementById("content").value;
-    let thumbnailPic = thumbnail;
 
     let formData = new FormData();
 
     formData.append("title", title);
     formData.append("content", content);
-    formData.append("thumbnail", thumbnailPic);
+    formData.append("thumbnail", thumbnail);
+    formData.append("media", media);
 
     axios.post("http://localhost:8000/articles/new", formData, {
       headers: { "Content-Type": "multipart/form-data" },
     });
   };
 
-  let handleFileChange = (e) => {
+  let handleThumbnailChange = (e) => {
     setThumbnail(e.target.files[0]);
-    console.log(thumbnail);
+  };
+
+  let handleMediaChange = (e) => {
+    setMedia(e.target.files[0]);
   };
 
   return (
     <div className="newArticle">
-      {console.log(thumbnail)}
       <form>
         <input type="text" name="title" id="title" />
         <textarea cols="30" rows="10" id="content"></textarea>
-        <input onChange={handleFileChange} type="file" name="thumbnail" />
+        <label>Thumbnail</label>
+        <input onChange={handleThumbnailChange} type="file" name="thumbnail" />
+        <label>Media</label>
+        <input onChange={handleMediaChange} type="file" name="media" />
         <button onClick={postArticle}>Send</button>
       </form>
     </div>
