e17f6c45a879b780e0af47bc641e12cc33120b0d
Luana Kimley <luanakimley@gmail.com>
2022-03-08 09:38:58 +0100

    change Articlechange Article\\

--- client/src/articles/Articles.js
+++ client/src/articles/Articles.js
@@ -1,39 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Articles.css";
 
-
 export default function Articles() {
-
-  const [data, setData] = useState( [] );
+  const [data, setData] = useState([]);
 
   async function getArticles() {
-    const data = (await axios.get('http://localhost:8000/articles')).data;
+    const data = (await axios.get("http://localhost:8000/articles")).data;
     setData(data);
   }
 
+  useEffect(() => {
+    getArticles();
+  }, []);
 
+  function displayThumbnail(url) {
+    return <img src={"http://localhost:8000/thumbnail/" + url} />;
+  }
 
-   useEffect(() => {
-    getArticles()
-   }, []);
+  function displayMedia(type, url) {
+    let fileType = type.split("/")[0];
+    if (fileType === "image") {
+      return <img src={"http://localhost:8000/media/" + url} />;
+    } else if (fileType === "video") {
+      return (
+        <video controls>
+          <source
+            src={"http://localhost:8000/media/" + url}
+            type={type}
+          ></source>
+        </video>
+      );
+    } else if (fileType === "audio") {
+      <audio controls>
+        <source src={"http://localhost:8000/media/" + url} type={type}></source>
+      </audio>;
+    }
+  }
 
-   function displayMedia(type, url) {
-     return <img src={"http://localhost:8000/media/" +url} />
-   }
-
-
-    return (
-       <>
-        <h1>
-            Articles !!
-        </h1>
-          {data.map( x =>  <article key={x.id}>
-                              <h1 className="Article_title">{x.title}</h1>
-                              <section dangerouslySetInnerHTML={{__html: x.content}}></section>
-                              {displayMedia(x.mediaType,x.mediaURL)}
-                           </article>
-           )}
-      </>
-    );
+  return (
+    <>
+      <h1>Articles !!</h1>
+      {data.map((x) => (
+        <article key={x.id}>
+          {displayThumbnail(x.thumbnailURL)}
+          <h1 className="Article_title">{x.title}</h1>
+          <section dangerouslySetInnerHTML={{ __html: x.content }}></section>
+          {displayMedia(x.mediaType, x.mediaURL)}
+          <button>Delete</button>
+        </article>
+      ))}
+    </>
+  );
 }
