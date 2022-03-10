b24aafcf90e86e2744ea14c7dfa5b909921ccda4
quentinhnrt <82041125+quentinhnrt@users.noreply.github.com>
2022-03-10 10:56:28 +0100

    User interface done

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,29 @@ import "./App.css";
 import NewArticle from "./articles/NewArticle";
 import DeleteArticle from "./articles/DeleteArticle";
 import Register from "./user/Register";
-import { Login } from "./user/Login";
-
+import Login, { ProtectedRoute, ProtectedLink, NotProtectedLink } from "./Login";
+import { useCookies, withCookies } from 'react-cookie';
 
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(['login']);
+
+
+  function disconnect(e) {
+    e.preventDefault()
+    removeCookie('login');
+  }
+
+
+
   return (
     <>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/articles">News</Link>
-        <Link to="/articles/new">New Article</Link>
-        <Link to="/user/register">Register</Link>
-        <Link to="/user/login">Login</Link>
+        <ProtectedLink to="/articles/new">New Article</ProtectedLink>
+        <NotProtectedLink to="/user/register">Register</NotProtectedLink>
+        <NotProtectedLink to="/user/login">Login</NotProtectedLink>
+        <ProtectedLink to="/" onClick={(e) => disconnect(e)}>Logout</ProtectedLink>
       </nav>
 
       <Routes>
@@ -27,7 +38,7 @@ function App() {
           path="/articles/delete/:id"
           element={<DeleteArticle />}
         />
-        <Route exact={true} path="/articles/new" element={<NewArticle />} />
+        <Route exact={true} path="/articles/new" element={<ProtectedRoute><NewArticle /></ProtectedRoute>} />
         <Route exact={true} path="/user/register" element={<Register />} />
         <Route exact={true} path="/user/login" element={<Login />} />
         <Route path="*" element={() => <p>Page Not Found</p>} />
