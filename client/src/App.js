1ecc193c736fa5cbdf1f03ff141e21ca5a6a93f8
quentinhnrt <82041125+quentinhnrt@users.noreply.github.com>
2022-03-10 11:45:09 +0100

    refresh

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,12 @@ import DeleteArticle from "./articles/DeleteArticle";
 import Register from "./user/Register";
 import Login, { ProtectedRoute, ProtectedLink, NotProtectedLink } from "./Login";
 import { useCookies, withCookies } from 'react-cookie';
+import { useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   const [cookies, setCookie, removeCookie] = useCookies(['login']);
-
+  
 
   function disconnect(e) {
     e.preventDefault()
@@ -18,17 +20,26 @@ function App() {
   }
 
 
-
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/articles">News</Link>
-        <ProtectedLink to="/articles/new">New Article</ProtectedLink>
-        <NotProtectedLink to="/user/register">Register</NotProtectedLink>
-        <NotProtectedLink to="/user/login">Login</NotProtectedLink>
-        <ProtectedLink to="/" onClick={(e) => disconnect(e)}>Logout</ProtectedLink>
-      </nav>
+      <div className="header">
+        <div className="container">
+          <img src="logo.png" alt="logo" className="logo" />
+        </div>
+
+      </div>
+      <div className="menu">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/articles">News</Link>
+          <ProtectedLink to="/articles/new">New Article</ProtectedLink>
+          <NotProtectedLink to="/user/register">Register</NotProtectedLink>
+          <NotProtectedLink to="/user/login">Login</NotProtectedLink>
+          <ProtectedLink to="/" onClick={(e) => disconnect(e)}>Logout</ProtectedLink>
+        </nav>
+
+      </div>
+
 
       <Routes>
         <Route exact={true} path="/" element={<Home />} />
