48d1e45b64166b1b0b35e8c60b4cf7eee9a9a692
Luana Kimley <luanakimley@gmail.com>
2022-03-10 11:32:42 +0100

    new article, tags, add tag to article

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Articles from "./articles/Articles";
 import "./App.css";
 import NewArticle from "./articles/NewArticle";
 import DeleteArticle from "./articles/DeleteArticle";
-
+import EditArticle from "./articles/EditArticle";
+import Tags from "./tags/Tags";
+import AddTagToArticle from "./tags/AddTagToArticle";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/articles">News</Link>
         <Link to="/articles/new">New Article</Link>
+        <Link to="/tags">Tags</Link>
       </nav>
 
       <Routes>
@@ -23,8 +26,18 @@ function App() {
           path="/articles/delete/:id"
           element={<DeleteArticle />}
         />
+        <Route
+          exact={true}
+          path="/articles/edit/:id"
+          element={<EditArticle />}
+        />
         <Route exact={true} path="/articles/new" element={<NewArticle />} />
-
+        <Route exact={true} path="/tags" element={<Tags />} />
+        <Route
+          exact={true}
+          path="/addarticletag/:id"
+          element={<AddTagToArticle />}
+        />
         <Route path="*" element={() => <p>Page Not Found</p>} />
       </Routes>
     </>
