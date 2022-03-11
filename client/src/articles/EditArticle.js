21d1598969f92b99f3645b02256f746b6420ec0a
Luana Kimley <luanakimley@gmail.com>
2022-03-11 14:00:05 +0100

    edit article fix

--- client/src/articles/EditArticle.js
+++ client/src/articles/EditArticle.js
@@ -7,6 +7,8 @@ export default function EditArticle() {
   const [content, setContent] = useState("");
   const [thumbnail, setThumbnail] = useState([]);
   const [media, setMedia] = useState([]);
+  const [mediaType, setMediaType] = useState([]);
+  const [leadStory, setLeadStory] = useState(1);
 
   let { id } = useParams();
 
@@ -16,9 +18,10 @@ export default function EditArticle() {
     ).data[0];
     setTitle(data.title);
     setContent(data.content);
-    console.log(data);
     setThumbnail(data.thumbnailURL);
     setMedia(data.mediaURL);
+    setLeadStory(data.leadStory);
+    setMediaType(data.mediaType);
   }
 
   useEffect(() => {
@@ -36,6 +39,7 @@ export default function EditArticle() {
     formData.append("content", content);
     formData.append("thumbnail", thumbnail);
     formData.append("media", media);
+    formData.append("leadStory", leadStory);
 
     axios.put(`http://localhost:8000/editarticle/${id}`, formData, {
       headers: { "Content-Type": "multipart/form-data" },
@@ -58,28 +62,166 @@ export default function EditArticle() {
     setMedia(e.target.files[0]);
   };
 
-  return (
-    <div className="newArticle">
-      <form>
-        <input
-          onChange={handleTitleChange}
-          type="text"
-          name="title"
-          value={title}
-          id="title"
+  function validateTitle() {
+    return title !== "";
+  }
+
+  function validateContent() {
+    return content !== "";
+  }
+
+  function validateThumbnail() {
+    return thumbnail.length !== 0;
+  }
+
+  function validate() {
+    return {
+      title: validateTitle(),
+      content: validateContent(),
+      thumbnail: validateThumbnail(),
+    };
+  }
+
+  function displayThumbnail(url) {
+    return (
+      <img className="w-100" src={"http://localhost:8000/thumbnail/" + url} />
+    );
+  }
+
+  function displayMedia(type, url) {
+    let fileType = type.split("/")[0];
+    if (fileType === "image") {
+      return (
+        <img
+          className="w-100 rounded"
+          src={"http://localhost:8000/media/" + url}
         />
-        <textarea
-          onChange={handleContentChange}
-          cols="30"
-          rows="10"
-          value={content}
-          id="content"
-        ></textarea>
-        <label>Thumbnail</label>
-        <input onChange={handleThumbnailChange} type="file" name="thumbnail" />
-        <label>Media</label>
-        <input onChange={handleMediaChange} type="file" name="media" />
-        <button onClick={postArticle}>Send</button>
+      );
+    } else if (fileType === "video") {
+      return (
+        <video className="w-100 rounded" controls>
+          <source
+            src={"http://localhost:8000/media/" + url}
+            type={type}
+          ></source>
+        </video>
+      );
+    } else if (fileType === "audio") {
+      <audio className="w-100 rounded" controls>
+        <source src={"http://localhost:8000/media/" + url} type={type}></source>
+      </audio>;
+    }
+  }
+
+  const inputsAreAllValid = Object.keys(validate()).every(
+    (index) => validate()[index]
+  );
+
+  let titleError = "";
+  let contentError = "";
+  let thumbnailError = "";
+
+  if (!validateTitle()) {
+    titleError = <p className="text-danger mt-2">Title is required.</p>;
+  }
+
+  if (!validateContent()) {
+    contentError = <p className="text-danger mt-2">Content is required.</p>;
+  }
+
+  if (!validateThumbnail()) {
+    thumbnailError = <p className="text-danger mt-2">Thumbnail is required.</p>;
+  }
+
+  return (
+    <div className="card m-3 shadow">
+      <div className="card-header">
+        <h1 className="text-center">Edit Article</h1>
+      </div>
+      <form className="p-3">
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            onChange={handleTitleChange}
+            className="form-control"
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+          />
+          {titleError}
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea
+            className="form-control"
+            cols="30"
+            rows="10"
+            id="content"
+            onChange={handleContentChange}
+            value={content}
+          ></textarea>
+          {contentError}
+        </div>
+        <div className="form-group">
+          <label>Thumbnail</label>
+          <div className="custom-file">
+            <input
+              onChange={handleThumbnailChange}
+              type="file"
+              className="custom-file-input"
+            />
+            <label className="custom-file-label">
+              {thumbnail ? thumbnail.name : "Choose file"}
+            </label>
+            {thumbnailError}
+          </div>
+          {displayThumbnail(thumbnail)}
+        </div>
+        <div className="form-group">
+          <label>Media</label>
+          <div className="custom-file">
+            <input
+              onChange={handleMediaChange}
+              type="file"
+              name="media"
+              className="form-control-file"
+            />
+            <label className="custom-file-label">
+              {media ? media.name : "Choose file"}
+            </label>
+          </div>
+          {mediaType.length ? displayMedia(mediaType, media) : null}
+        </div>
+
+        <label className="form-check-label mr-3">Lead story?</label>
+        <div className="form-check form-check-inline">
+          <input
+            type="radio"
+            name="leadStory"
+            checked={leadStory === 1}
+            onClick={() => setLeadStory(1)}
+          />
+          <span className="form-check-label ml-1">Yes</span>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            type="radio"
+            name="leadStory"
+            checked={leadStory === 0}
+            onClick={() => setLeadStory(0)}
+          />
+          <span className="form-check-label ml-1">No</span>
+        </div>
+
+        <button
+          className="btn btn-block btn-outline-success mt-4"
+          onClick={postArticle}
+          disabled={!inputsAreAllValid}
+        >
+          Send
+        </button>
       </form>
     </div>
   );
